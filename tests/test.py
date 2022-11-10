76e242baf1f3645fbc0cc1049baab4767ce67423
owsei <owseiwastaken@gmail.com>
2022-11-10 11:24:40 -0300

    tests dir

--- tests/test.py
+++ tests/test.py
@@ -2,14 +2,31 @@
 #imports
 from sys import exit
 
+#simple in-out-return tests
+
+#in
+# don't
+#out
+# don't
+# don't hi
+#return
+# 0
+
+#in
+# ?
+#out
+# uwu
+#return
+# 1
 
 #main
 def Main() -> int:
 	x = input("")
 	if x == "don't":
 		print(x)
-		print(x+"hi")
+		print(x+" hi")
 		return 0
+	print("uwu")
 	return 1
 
 #start
